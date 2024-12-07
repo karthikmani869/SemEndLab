838c700e2cfc9d1d90917afa19d5c5eb8b9eaeca
MEDA KARTHIK MANI <116943960+karthikmani869@users.noreply.github.com>
2024-12-07 12:34:50 +0530

    Add files via upload

---
+++ SemEnd/src/main/java/com/klef/jfsd/exam/CustomerService.java
@@ -0,0 +1,7 @@
+package com.klef.jfsd.exam;
+
+import java.util.List;
+
+public interface CustomerService {
+    List<Customer> getAllCustomers();
+}
---
+++ SemEnd/src/main/java/com/klef/jfsd/exam/CustomerServiceImpl.java
@@ -0,0 +1,19 @@
+package com.klef.jfsd.exam;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+}
