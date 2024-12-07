d85ed76b284bb8bff05fa30c4749dd02b1710ab6
MEDA KARTHIK MANI <116943960+karthikmani869@users.noreply.github.com>
2024-12-07 12:30:02 +0530

    Add files via upload

---
+++ SemEnd/src/main/java/com/example/demo/SemEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemEndApplication.class, args);
+	}
+
+}
